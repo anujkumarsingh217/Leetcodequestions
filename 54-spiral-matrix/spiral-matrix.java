f9487a8694d3f0e45de3e688da8af06247fde5ea
anujkumarsingh217 <anujkumarsingh6721@gmail.com>
2026-08-17 21:11:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (80.65%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,35 +1,40 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
        List<Integer>ans=new ArrayList<>();
-       int top=0;
-       int bottom=matrix.length-1;
-       int left=0;
-       int right=matrix[0].length-1;
-       while(top<=bottom && left<=right){
-           for(int j=left;j<=right;j++){
-            ans.add(matrix[top][j]);
-           }
-           top++;
+        int m=matrix.length;
+        int n=matrix[0].length;
 
-           for(int i=top;i<=bottom;i++){
-                ans.add(matrix[i][right]);
-           }
-           right--;
+        int sr=0;
+        int er=m-1;
+        int sc=0;
+        int ec=n-1;
 
-           if(top<=bottom){
-            for(int j=right;j>=left;j--){
-                ans.add(matrix[bottom][j]);
+        while(sr<=er && sc<=ec){
+            for(int col=sc;col<=ec;col++){
+                ans.add(matrix[sr][col]);
             }
-            bottom--;
-           }
-
-           if(left<=right){
-            for(int i=bottom;i>=top;i--){
-                ans.add(matrix[i][left]);
+            sr++;
+            for(int row=sr;row<=er;row++){
+                ans.add(matrix[row][ec]);
             }
-            left++;
-           }
-       }  
-            return ans;
+            ec--;
+
+            if(sr<=er){
+                for(int col=ec;col>=sc;col--){
+                    ans.add(matrix[er][col]);
+                }
+                er--;
+            }
+
+            if(sc<=ec){
+                for(int row=er;row>=sr;row--){
+                    ans.add(matrix[row][sc]);
+                }
+                sc++;
+            }
+
+        }
+        return ans;
+
     }
 }
