62330cdfeb036d7722757d6d0724aa26d3f86783
anujkumarsingh217 <anujkumarsingh6721@gmail.com>
2026-08-18 23:39:57 +0530

    Force committed solution - Time: O(n^2), Space: O(1)

---
+++ 700143-bubble-sort-GFG/700143-bubble-sort.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void bubbleSort(int[] arr) {
+        // code here
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
