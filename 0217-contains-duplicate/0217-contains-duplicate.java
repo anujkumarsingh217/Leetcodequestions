eb37aecb8e9ba892ed02f9574f337a8ef992e946
anujkumarsingh217 <anujkumarsingh6721@gmail.com>
2026-08-26 11:52:35 +0530

    Time: 19 ms (48.7%), Space: 108.1 MB (56.51%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer>set=new HashSet<>();
+        for(int n:nums){
+            if(set.contains(n)){
+                return true;
+            }
+            set.add(n);
+        }
+        return false;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
