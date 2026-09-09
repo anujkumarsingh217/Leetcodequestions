359cddd2332399da48fd14f35ff261e450f0516e
anujkumarsingh217 <anujkumarsingh6721@gmail.com>
2026-09-09 13:35:34 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (72.62%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode a = head;  // slow
+        ListNode b = head;  // fast
+
+        while (b != null && b.next != null) {
+            a = a.next;          // 1 step
+            b = b.next.next;     // 2 steps
+
+            if (a == b) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
