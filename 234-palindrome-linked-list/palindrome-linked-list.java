260e9dca33ac952c952c4c939e936ea2000234f2
anujkumarsingh217 <anujkumarsingh6721@gmail.com>
2026-09-19 01:29:37 +0530

    Time: 6 ms (33.99%) | Memory: 94.4 MB (67.11%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,71 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode getMidPoint(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null){
+            fast=fast.next;
+            if(fast!=null){
+                fast=fast.next;
+                slow=slow.next;
+            }
+        }
+        return slow;
+    }
+    public ListNode reverseLL(ListNode head){
+        ListNode prev=null;
+        ListNode curr=head;
+        while(curr!=null){
+            ListNode forward=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=forward;
+        }
+        return prev;
+    }
+    public boolean isPalindrome(ListNode head) {
+        if(head==null){
+            return true;
+        }
+
+        if(head.next==null){
+            return true;
+        }
+        ListNode list2=getMidPoint(head);
+        ListNode temp=head;
+
+        while(temp.next!=list2){
+            temp=temp.next;
+        }
+        temp.next=null;
+
+        ListNode head2=reverseLL(list2);
+
+        ListNode temp1=head;
+        ListNode temp2=head2;
+
+        while(temp1!=null && temp2!=null){
+            if(temp1.val!=temp2.val){
+                return false;
+            }
+            else{
+                temp1=temp1.next;
+                temp2=temp2.next;
+            }
+        }
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
